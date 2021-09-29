14b372e728cfa1ad24e5ec528047029005c15317
Jengstrom124 <engstromj124@gmail.com>
2021-09-29 18:11:13 +1000

    Player Movement WIP

---
+++ Assets/John/TestActionMap.cs
@@ -0,0 +1,243 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/John/TestActionMap.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @TestActionMap : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @TestActionMap()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""TestActionMap"",
+    ""maps"": [
+        {
+            ""name"": ""InGame"",
+            ""id"": ""47d8e587-65e6-4d54-90e5-efde1284e6a9"",
+            ""actions"": [
+                {
+                    ""name"": ""Movement"",
+                    ""type"": ""Value"",
+                    ""id"": ""e7a95867-76a8-424d-bc5a-cf11ee1db597"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": ""2D Vector"",
+                    ""id"": ""544449ba-177d-48e1-92f7-4b457a5fef5c"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""9360bb4f-3a45-4b9e-b0c5-86ff9b84810f"",
+                    ""path"": ""<Keyboard>/w"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""d530943e-ebac-4e82-ba97-0bf767b5b818"",
+                    ""path"": ""<Keyboard>/s"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""27ed8100-fba8-42eb-89fc-999b5f51ba0d"",
+                    ""path"": ""<Keyboard>/a"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""41be0b57-41d4-4df4-b107-9b2e5b06f3f0"",
+                    ""path"": ""<Keyboard>/d"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                }
+            ]
+        },
+        {
+            ""name"": ""InMenu"",
+            ""id"": ""359df84c-0240-4720-8f6a-417631dd4d82"",
+            ""actions"": [
+                {
+                    ""name"": ""New action"",
+                    ""type"": ""Button"",
+                    ""id"": ""93a164d4-5d81-4d10-95ea-dcab181bf9ff"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": """",
+                    ""id"": ""a9086ccc-b825-447c-b63f-a13d426c4dd5"",
+                    ""path"": """",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""New action"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": []
+}");
+        // InGame
+        m_InGame = asset.FindActionMap("InGame", throwIfNotFound: true);
+        m_InGame_Movement = m_InGame.FindAction("Movement", throwIfNotFound: true);
+        // InMenu
+        m_InMenu = asset.FindActionMap("InMenu", throwIfNotFound: true);
+        m_InMenu_Newaction = m_InMenu.FindAction("New action", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // InGame
+    private readonly InputActionMap m_InGame;
+    private IInGameActions m_InGameActionsCallbackInterface;
+    private readonly InputAction m_InGame_Movement;
+    public struct InGameActions
+    {
+        private @TestActionMap m_Wrapper;
+        public InGameActions(@TestActionMap wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Movement => m_Wrapper.m_InGame_Movement;
+        public InputActionMap Get() { return m_Wrapper.m_InGame; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(InGameActions set) { return set.Get(); }
+        public void SetCallbacks(IInGameActions instance)
+        {
+            if (m_Wrapper.m_InGameActionsCallbackInterface != null)
+            {
+                @Movement.started -= m_Wrapper.m_InGameActionsCallbackInterface.OnMovement;
+                @Movement.performed -= m_Wrapper.m_InGameActionsCallbackInterface.OnMovement;
+                @Movement.canceled -= m_Wrapper.m_InGameActionsCallbackInterface.OnMovement;
+            }
+            m_Wrapper.m_InGameActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Movement.started += instance.OnMovement;
+                @Movement.performed += instance.OnMovement;
+                @Movement.canceled += instance.OnMovement;
+            }
+        }
+    }
+    public InGameActions @InGame => new InGameActions(this);
+
+    // InMenu
+    private readonly InputActionMap m_InMenu;
+    private IInMenuActions m_InMenuActionsCallbackInterface;
+    private readonly InputAction m_InMenu_Newaction;
+    public struct InMenuActions
+    {
+        private @TestActionMap m_Wrapper;
+        public InMenuActions(@TestActionMap wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Newaction => m_Wrapper.m_InMenu_Newaction;
+        public InputActionMap Get() { return m_Wrapper.m_InMenu; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(InMenuActions set) { return set.Get(); }
+        public void SetCallbacks(IInMenuActions instance)
+        {
+            if (m_Wrapper.m_InMenuActionsCallbackInterface != null)
+            {
+                @Newaction.started -= m_Wrapper.m_InMenuActionsCallbackInterface.OnNewaction;
+                @Newaction.performed -= m_Wrapper.m_InMenuActionsCallbackInterface.OnNewaction;
+                @Newaction.canceled -= m_Wrapper.m_InMenuActionsCallbackInterface.OnNewaction;
+            }
+            m_Wrapper.m_InMenuActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Newaction.started += instance.OnNewaction;
+                @Newaction.performed += instance.OnNewaction;
+                @Newaction.canceled += instance.OnNewaction;
+            }
+        }
+    }
+    public InMenuActions @InMenu => new InMenuActions(this);
+    public interface IInGameActions
+    {
+        void OnMovement(InputAction.CallbackContext context);
+    }
+    public interface IInMenuActions
+    {
+        void OnNewaction(InputAction.CallbackContext context);
+    }
+}
--- Assets/Scripts/PlayerMovementModel.cs
+++ Assets/Scripts/PlayerMovementModel.cs
@@ -7,8 +7,20 @@ public class PlayerMovementModel : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        TestActionMap testActionMap = new TestActionMap();
+        testActionMap.InGame.Enable();
+
+        //testActionMap.InGame.Movement.performed += PlayerMovement;
     }
+
+    //NEED TO FIX
+    //void PlayerMovement(InputAction.CallbackContext obj)
+    //{
+       // Debug.Log(obj.ReadValue<Vector2>());
+   // }
+
+
+
 
     // Update is called once per frame
     void Update()
