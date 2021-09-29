6c2698eee9134fc7c4699f26bbd721306bf94238
Tom Bartle <tombartle96@gmail.com>
2021-09-29 10:05:50 +1000

    Added input actions test

---
+++ Assets/Tom/ActionTest.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ActionTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        TomActionTest actions = new TomActionTest();
+        actions.InGame.Enable();
+
+        actions.InGame.Movement.performed += MoveOnPerformed;
+    }
+
+    private void MoveOnPerformed(InputAction.CallbackContext obj)
+    {
+        Debug.Log(obj.ReadValue<Vector2>());
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Tom/Tom Action Test.cs
@@ -0,0 +1,176 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/Tom/Tom Action Test.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @TomActionTest : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @TomActionTest()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""Tom Action Test"",
+    ""maps"": [
+        {
+            ""name"": ""In Game"",
+            ""id"": ""a9f3b7dc-2f84-40f2-acda-5123f81a7f77"",
+            ""actions"": [
+                {
+                    ""name"": ""Movement"",
+                    ""type"": ""Value"",
+                    ""id"": ""40025090-9119-4db7-9cd1-fcd1766ce49c"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": ""2D Vector"",
+                    ""id"": ""01b67519-9a7f-4d81-b002-f97b7be4e803"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""898e0e59-a035-4d23-853b-86baa2f9c234"",
+                    ""path"": ""<Keyboard>/w"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""8036d54f-5937-4f43-9f87-aa9ab81b2933"",
+                    ""path"": ""<Keyboard>/s"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""2e02e318-9476-4ade-9063-806ec06b208f"",
+                    ""path"": ""<Keyboard>/a"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""de1fe50e-ef24-4585-a8ca-ff9370aa09aa"",
+                    ""path"": ""<Keyboard>/d"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": []
+}");
+        // In Game
+        m_InGame = asset.FindActionMap("In Game", throwIfNotFound: true);
+        m_InGame_Movement = m_InGame.FindAction("Movement", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // In Game
+    private readonly InputActionMap m_InGame;
+    private IInGameActions m_InGameActionsCallbackInterface;
+    private readonly InputAction m_InGame_Movement;
+    public struct InGameActions
+    {
+        private @TomActionTest m_Wrapper;
+        public InGameActions(@TomActionTest wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Movement => m_Wrapper.m_InGame_Movement;
+        public InputActionMap Get() { return m_Wrapper.m_InGame; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(InGameActions set) { return set.Get(); }
+        public void SetCallbacks(IInGameActions instance)
+        {
+            if (m_Wrapper.m_InGameActionsCallbackInterface != null)
+            {
+                @Movement.started -= m_Wrapper.m_InGameActionsCallbackInterface.OnMovement;
+                @Movement.performed -= m_Wrapper.m_InGameActionsCallbackInterface.OnMovement;
+                @Movement.canceled -= m_Wrapper.m_InGameActionsCallbackInterface.OnMovement;
+            }
+            m_Wrapper.m_InGameActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Movement.started += instance.OnMovement;
+                @Movement.performed += instance.OnMovement;
+                @Movement.canceled += instance.OnMovement;
+            }
+        }
+    }
+    public InGameActions @InGame => new InGameActions(this);
+    public interface IInGameActions
+    {
+        void OnMovement(InputAction.CallbackContext context);
+    }
+}
