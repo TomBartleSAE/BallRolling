0d64d07ba2f950449fd26c6ee67f91f4e1c2841a
Tom Bartle <tombartle96@gmail.com>
2021-11-20 14:13:33 +1100

    Fixed hammer physics

--- Assets/John/TestScripts/Environment Scripts/SpinningObstacle.cs
+++ Assets/John/TestScripts/Environment Scripts/SpinningObstacle.cs
@@ -13,13 +13,14 @@ public class SpinningObstacle : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        rb.centerOfMass = Vector3.zero;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(Vector3.up, (rotateSpeed*2) * Time.deltaTime);
+        //transform.Rotate(Vector3.up, (rotateSpeed*2) * Time.deltaTime);
 
-        //rb.AddTorque(Vector3.up * rotateSpeed, ForceMode.VelocityChange);
+        rb.AddTorque(Vector3.up * rotateSpeed, ForceMode.VelocityChange);
     }
 }
